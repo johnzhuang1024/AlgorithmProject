13c230a6bd5c14752659b98c92b314fc8790b6b1
JohnZhuang <johnzhuang1024@gmail.com>
2023-02-16 10:14:49 +0800

    Code01_PreSum

--- AlgorithmProject/src/main/java/com/JohnZhuang/AlgorighmTest/class02/Code01_PreSum.java
+++ AlgorithmProject/src/main/java/com/JohnZhuang/AlgorighmTest/class02/Code01_PreSum.java
@@ -7,6 +7,31 @@ package com.JohnZhuang.AlgorighmTest.class02;
  * @Version: 1.0
  */
 public class Code01_PreSum {
+
+    public static void main(String[] args){
+
+        int[] ints = {1,2,3,4,8,6,3,4,6,8,4};
+
+        RangeSum1 rangeSum1 = new RangeSum1(ints);
+        RangeSum2 rangeSum2 = new RangeSum2(ints);
+
+        long start1 = System.nanoTime();
+        System.out.println(rangeSum1.rangeSum(1, 5));
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        System.out.println(rangeSum2.rangeSum(1, 5));
+        long end2 = System.nanoTime();
+
+        System.out.println(end1-start1);// 147100
+        System.out.println(end2-start2);// 20800
+
+
+
+
+
+    }
+
     public static class RangeSum1 {
 
         private int[] arr;
